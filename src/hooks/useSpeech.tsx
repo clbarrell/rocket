3e7d8b1af0aea7e744b02055633408146a45cf14
Chris <clbarrell@gmail.com>
2023-04-19 16:39:07 +1000

    fix voices loading in chrome

--- src/hooks/useSpeech.tsx
+++ src/hooks/useSpeech.tsx
@@ -1,3 +1,4 @@
+import log from "@/lib/log";
 import { useEffect, useState } from "react";
 
 export const useSpeech = () => {
@@ -7,17 +8,32 @@ export const useSpeech = () => {
 
   useEffect(() => {
     // load voices
-    const realVoices = window.speechSynthesis.getVoices();
-    setVoices(realVoices);
-    setspeechUtterance(new SpeechSynthesisUtterance());
+    log("setting event listener");
+    const firstGo = window.speechSynthesis.getVoices();
+    console.log("first go", firstGo.length);
+
+    window.speechSynthesis.onvoiceschanged = () => {
+      log("set voices onChange");
+      const realVoices = window.speechSynthesis.getVoices();
+      setVoices(realVoices);
+    };
   }, []);
+
+  useEffect(() => {
+    if (voices.length > 0) {
+      const speech = new SpeechSynthesisUtterance();
+      speech.lang = "en-US";
+      log("voices length", voices.length);
+      speech.voice = voices[0];
+      speech.rate = 0.85;
+      setspeechUtterance(speech);
+    }
+  }, [voices]);
 
   const speak = ({ text }: { text: string }) => {
     if ("speechSynthesis" in window && speechUtterance != null) {
       // Set the text to be spoken
       speechUtterance.text = text;
-      speechUtterance.voice = voices[0];
-      speechUtterance.rate = 0.85;
       window.speechSynthesis.speak(speechUtterance);
     } else {
       console.error("Speech synthesis is not supported in this browser.");
